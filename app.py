9199f6a6e2095a41f80ec234d2e21187e347b4c4
edwardyipck <73915962+edwardyipck@users.noreply.github.com>
2020-12-19 15:12:42 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import pandas as pd
-import itertools as it
 import dash
 import dash_table
 import plotly.express as px
@@ -8,36 +6,15 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 
-# Creating the dataframe
 
-drivers = pd.read_csv("assets/data/drivers.csv")
-bodies = pd.read_csv("assets/data/bodies.csv")
-tires = pd.read_csv("assets/data/tires.csv")
-gliders = pd.read_csv("assets/data/gliders.csv")
+import dataframe
 
-namelist = [drivers.Driver.tolist(),bodies.Body.tolist(),tires.Tires.tolist(),gliders.Glider.tolist()]
-allnames = list(it.product(*namelist))
-statlist = [drivers.drop("Driver",axis=1).values.tolist(),
-            bodies.drop("Body",axis=1).values.tolist(),
-            tires.drop("Tires",axis=1).values.tolist(),
-            gliders.drop("Glider",axis=1).values.tolist()]
-allstats = list(it.product(*statlist))
-allstats = [[sum(x) for x in zip(i[0],i[1],i[2],i[3])] for i in allstats]
-nametable = pd.DataFrame(allnames, columns = ["Driver", "Body", "Tires","Glider"])
-stattable = pd.DataFrame(allstats, columns = ['Weight', 'Acceleration', 'On-Road traction', '(Off-Road) Traction',
-                                              'Mini-Turbo', 'Ground Speed', 'Water Speed', 'Anti-Gravity Speed',
-                                              'Air Speed', 'Ground Handling', 'Water Handling',
-                                              'Anti-Gravity Handling', 'Air Handling'])
-comb = nametable.join(stattable)
-comb["Total"] = comb.sum(axis=1)
-
-# Dash Application
-
+comb, driver_list = dataframe.dataframe()
 cols = ['Driver', 'Body', 'Tires', 'Glider', 'WG', 'AC', 'ON', 'OF', 'MT', 'SL', 'SW', 'SA', 'SG', 'TL', 'TW', 'TA', 'TG', 'Total']
 app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
-fig = px.scatter(comb.copy()[["Acceleration","Ground Speed"]].drop_duplicates(), x="Acceleration", y="Ground Speed",
+fig = px.scatter(comb[["Acceleration","Ground Speed"]].drop_duplicates(), x="Acceleration", y="Ground Speed",
                      hover_data=["Acceleration", "Ground Speed"],
                      range_x=[-0.5, 21],range_y=[-0.5, 21])
 
@@ -73,7 +50,7 @@ app.layout = html.Div([
              
             dbc.Col(html.Div(
                     dcc.Dropdown(id="slct_driver",
-                        options=[{"label": i, "value": i} for i in drivers["Driver"].to_list()],
+                        options=[{"label": i, "value": i} for i in driver_list],
                         multi=True,
                         clearable=False,
                         placeholder="All Drivers",),
@@ -215,9 +192,9 @@ def update_table(clickData,ycol,xcol,scol,sval,driver,sortby):
 def update_graph(ycol,xcol,scol,sval,driver):
     if all(i != None for i in [ycol,xcol,scol]) and len(set([ycol,xcol,scol])) ==3:
         if driver!=None and driver != []:
-            data=comb[comb["Driver"].isin(driver)].copy()
+            data=comb[comb["Driver"].isin(driver)]
         else:
-            data=comb.copy()
+            data=comb
         data = data[[ycol,xcol,scol]].drop_duplicates()
         data = data[data[scol].between(sval[0],sval[1])]
         fig = px.scatter(data, x=xcol, y=ycol,color = scol, range_color=[0,20],
