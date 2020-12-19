6063cd6edd8bd69c667fa005254996d1c520443e
edwardyipck <73915962+edwardyipck@users.noreply.github.com>
2020-12-19 21:33:09 +0000

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -2,21 +2,29 @@ import pandas as pd
 import itertools as it
 
 
+
 def dataframe():
     drivers = pd.read_csv("assets/data/drivers.csv")
     bodies = pd.read_csv("assets/data/bodies.csv")
     tires = pd.read_csv("assets/data/tires.csv")
     gliders = pd.read_csv("assets/data/gliders.csv")
     
-    namelist = [drivers.Driver.tolist(),bodies.Body.tolist(),tires.Tires.tolist(),gliders.Glider.tolist()]
-    namelist = list(it.product(*namelist))
+    driver_list = drivers["Driver"].to_list()
+    
+    namelist = list(it.product(*[drivers.Driver.tolist(),
+                                 bodies.Body.tolist(),
+                                 tires.Tires.tolist(),
+                                 gliders.Glider.tolist()]
+                              )
+                   )
     namelist = pd.DataFrame(namelist, columns = ["Driver", "Body", "Tires", "Glider"])
     
-    statlist = [drivers.drop("Driver",axis=1).values.tolist(),
-                bodies.drop("Body",axis=1).values.tolist(),
-                tires.drop("Tires",axis=1).values.tolist(),
-                gliders.drop("Glider",axis=1).values.tolist()]
-    statlist = list(it.product(*statlist))
+    statlist = list(it.product(*[drivers.drop("Driver",axis=1).values.tolist(),
+                            bodies.drop("Body",axis=1).values.tolist(),
+                            tires.drop("Tires",axis=1).values.tolist(),
+                            gliders.drop("Glider",axis=1).values.tolist()]
+                          )
+               )
     statlist = [[sum(x) for x in zip(i[0],i[1],i[2],i[3])] for i in statlist]
     statlist = pd.DataFrame(statlist, columns = ['Weight', 'Acceleration', 'On-Road traction', '(Off-Road) Traction',
                                                   'Mini-Turbo', 'Ground Speed', 'Water Speed', 'Anti-Gravity Speed',
@@ -25,5 +33,5 @@ def dataframe():
     
     namelist = namelist.join(statlist)
     namelist["Total"] = namelist.sum(axis=1)
-    driver_list = drivers["Driver"].to_list()
     return namelist, driver_list
+
