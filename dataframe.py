a41acf6eed622e3b91711d1769e18ed10c140604
edwardyipck <73915962+edwardyipck@users.noreply.github.com>
2020-12-20 16:53:42 +0000

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import itertools as it
 
-
-
 def dataframe():
-    drivers = pd.read_csv("assets/data/drivers.csv")
-    bodies = pd.read_csv("assets/data/bodies.csv")
-    tires = pd.read_csv("assets/data/tires.csv")
-    gliders = pd.read_csv("assets/data/gliders.csv")
+    datatypes = {0: 'object'}
+    datatypes.update({i:'uint8' for i in range(1,14)})
     
+    drivers = pd.read_csv("assets/data/drivers.csv",dtype=datatypes)
+    bodies = pd.read_csv("assets/data/bodies.csv",dtype=datatypes)
+    tires = pd.read_csv("assets/data/tires.csv",dtype=datatypes)
+    gliders = pd.read_csv("assets/data/gliders.csv",dtype=datatypes)
+
     driver_list = drivers["Driver"].to_list()
     
     namelist = list(it.product(*[drivers.Driver.tolist(),
@@ -17,7 +18,8 @@ def dataframe():
                                  gliders.Glider.tolist()]
                               )
                    )
-    namelist = pd.DataFrame(namelist, columns = ["Driver", "Body", "Tires", "Glider"])
+    
+    namelist = pd.DataFrame(namelist, columns = ["Driver", "Body", "Tires", "Glider"],dtype='category')
     
     statlist = list(it.product(*[drivers.drop("Driver",axis=1).values.tolist(),
                             bodies.drop("Body",axis=1).values.tolist(),
@@ -25,13 +27,15 @@ def dataframe():
                             gliders.drop("Glider",axis=1).values.tolist()]
                           )
                )
+    
     statlist = [[sum(x) for x in zip(i[0],i[1],i[2],i[3])] for i in statlist]
     statlist = pd.DataFrame(statlist, columns = ['Weight', 'Acceleration', 'On-Road traction', '(Off-Road) Traction',
                                                   'Mini-Turbo', 'Ground Speed', 'Water Speed', 'Anti-Gravity Speed',
                                                   'Air Speed', 'Ground Handling', 'Water Handling',
-                                                  'Anti-Gravity Handling', 'Air Handling'])
+                                                  'Anti-Gravity Handling', 'Air Handling'],dtype='uint8')
     
     namelist = namelist.join(statlist)
-    namelist["Total"] = namelist.sum(axis=1)
+    namelist["Total"] = namelist.sum(axis=1).astype('uint8')
+    
     return namelist, driver_list
 
