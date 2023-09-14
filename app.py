82e0d97146c35fdfff559fe9992172506365d556
edwardyipck <73915962+edwardyipck@users.noreply.github.com>
2023-09-14 17:02:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import dash
 import dash_table
 import plotly.express as px
 import dash_core_components as dcc
-import dash_html_components as html
+from dash import html
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 
